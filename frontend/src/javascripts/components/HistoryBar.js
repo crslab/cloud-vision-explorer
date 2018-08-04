42d5fc6ffa97cc0d2c51a871acc13dbd3bd6127a
Teekayu Klongtruajrok <teekayu8@hotmail.com>
2018-08-04 18:46:33 +0800

    Fix color stylings for history bar

--- frontend/src/javascripts/components/HistoryBar.js
+++ frontend/src/javascripts/components/HistoryBar.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-
-const style = {
-  testText: {
-    color: 'white',
-    backgroundColor: '#626262',
-    width: '100%',
-    height: '100%'
-  }
-}
+import Button from 'react-toolbox/lib/button'
+import 'stylesheets/HistoryBar'
 
 class HistoryBar extends Component {
   static get propTypes() {
@@ -21,8 +14,11 @@ class HistoryBar extends Component {
   render() {
     return (
       <div style={this.props.style}>
-        <div style={style.testText}>
-          SUCK MY BALLS
+        <div className="titleIcon">
+          <Button icon="collections" ripple inverse />
+        </div>
+        <div className="historyCollection">
+
         </div>
       </div>
     )
