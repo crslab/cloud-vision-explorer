8b569a833ec79c4d53f1df12bc7651aa09d3d4e7
seventwoten <a0085039@gmail.com>
2018-08-05 18:06:37 +0800

    Changed left-bar image set

--- frontend/src/javascripts/misc/Constants.js
+++ frontend/src/javascripts/misc/Constants.js
@@ -1,18 +1,31 @@
 const OPEN_IMAGE_BOOKMARK_IDS = [
-    {id: 'ppt1_001' },  // Cat
-    {id: 'ppt2_045' },  // Kangaroo
-    {id: 'ppt3_021' },  // Ethnic Clothes
-    {id: 'ppt1_009' },  // Citi Field
-    {id: 'ppt1_023' },  // Android
-    {id: 'ppt1_010' },  // node1 for trackTwoNodes test
-    {id: 'ppt3_035' },  // node2 for trackTwoNodes test
-    {id: 'ppt1_002' },  // select this for trackTwoNodes
-    {id: 'ppt1_004' },
-    {id: "ppt3_085" },
-    {id: "ppt4_011" },
-    {id: "ppt4_076" },
-    {id: "ppt4_099" },
-    {id: "ppt5_034" }
+    // {id: 'ppt1_001' },  // Cat
+    // {id: 'ppt2_045' },  // Kangaroo
+    // {id: 'ppt3_021' },  // Ethnic Clothes
+    // {id: 'ppt1_009' },  // Citi Field
+    // {id: 'ppt1_023' },  // Android
+    // {id: 'ppt1_010' },  // node1 for trackTwoNodes test
+    // {id: 'ppt3_035' },  // node2 for trackTwoNodes test
+    // {id: 'ppt1_002' },  // select this for trackTwoNodes
+    // {id: 'ppt1_004' },
+    // {id: "ppt3_085" },
+    // {id: "ppt4_011" },
+    // {id: "ppt4_076" },
+    // {id: "ppt4_099" },
+    // {id: "ppt5_034" }
+    
+    {id: "ppt1_001" },
+    {id: "ppt1_025" },
+    {id: "ppt1_043" },
+    {id: "ppt1_073" },
+    {id: "ppt1_100" },
+    {id: "ppt2_084" },
+    {id: "ppt2_089" },
+    {id: "ppt3_021" },
+    {id: "ppt4_046" },
+    {id: "ppt5_093" },
+    {id: "ppt5_098" },
+    {id: "ppt5_099" }
 ]
 
 export { OPEN_IMAGE_BOOKMARK_IDS}
