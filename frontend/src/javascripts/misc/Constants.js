c74943db3b3e323b717704ae7152ef40159ade8c
Weicong <sng.weicong@gmail.com>
2018-11-16 10:50:38 +0800

    Reduce number of initial samples to more interesting ones

--- frontend/src/javascripts/misc/Constants.js
+++ frontend/src/javascripts/misc/Constants.js
@@ -14,18 +14,12 @@ const OPEN_IMAGE_BOOKMARK_IDS = [
     // {id: "ppt4_099" },
     // {id: "ppt5_034" }
     
-    {id: "ppt1_001" },
     {id: "ppt1_025" },
-    {id: "ppt1_043" },
+    {id: "ppt4_046" },
     {id: "ppt1_073" },
-    {id: "ppt1_100" },
-    {id: "ppt2_084" },
     {id: "ppt2_089" },
     {id: "ppt3_021" },
-    {id: "ppt4_046" },
-    {id: "ppt5_093" },
     {id: "ppt5_098" },
-    {id: "ppt5_099" }
 ]
 
 export { OPEN_IMAGE_BOOKMARK_IDS}
